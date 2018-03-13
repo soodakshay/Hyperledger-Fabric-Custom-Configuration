af4ad727f2e285ab329561e2bc72f7c1163c2655
Akshay Sood <akshay.sood@live.com>
2018-03-13 09:25:13 +0530

    latest code

--- create-network.sh
+++ create-network.sh
@@ -1,12 +1,31 @@
+
+docker kill $(docker ps -q)
+docker rm $(docker ps -qa)
+docker network prune
+echo "y"
+
+echo "**************************** Docker containers killed and removed ****************************"
+
+rm -R channel-artifacts
+rm -R crypto-config
+
+echo "**************************** Old Certificates removed ****************************"
+
+mkdir channel-artifacts
+mkdir crypto-config
+
 cryptogen generate --config crypto-config.yaml
-configtxgen -profile Debut_Axis_Orderer_Genesis_Block -outputBlock ./channel-artifacts/genesis.block 
-export CHANNEL_NAME=channel1  && ../bin/configtxgen -profile Debut_Axis_Channel_Profile -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
-configtxgen -profile Debut_Axis_Channel_Profile -outputAnchorPeersUpdate ./channel-artifacts/DebutMSPanchors.tx -channelID $CHANNEL_NAME -asOrg Debut_Infotech_Pvt_Ltd
-configtxgen -profile Debut_Axis_Channel_Profile -outputAnchorPeersUpdate ./channel-artifacts/AxisMSPanchors.tx -channelID $CHANNEL_NAME -asOrg Axis_Bank_Pvt_Ltd
+
+echo "**************************** New Certificates Generated ****************************"
+
+export FABRIC_CFG_PATH=$PWD
 export IMAGE_TAG=latest
+export COMPOSE_PROJECT_NAME=first_project
+
+configtxgen -profile DebutAxisGenesisBlockProfile -outputBlock ./channel-artifacts/genesis.block
+
+export CHANNEL_NAME=channel1  && configtxgen -profile DebutAxisChannelProfile -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
+
+configtxgen -profile DebutAxisChannelProfile -outputAnchorPeersUpdate ./channel-artifacts/DebutMSPanchors.tx -channelID $CHANNEL_NAME -asOrg DebutInfotechPvtLtd
+configtxgen -profile DebutAxisChannelProfile -outputAnchorPeersUpdate ./channel-artifacts/AxisMSPanchors.tx -channelID $CHANNEL_NAME -asOrg AxisBankPvtLtd
 docker-compose -f docker-compose-cli.yaml up
-
-
-
-
-
