7e929cb59228953b6efa44772f7172837ac21039
Akshay Sood <akshay.sood@live.com>
2018-03-26 18:40:11 +0530

    network with own chaincode

---
+++ chaincode/mychaincode.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"strconv"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/protos/peer"
+)
+
+type MyContract struct {
+}
+
+type User struct {
+	RollNo    int64  `json:"rollNo"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+/*
+ * Initialize ledger
+ */
+func (contract *MyContract) Init(stub shim.ChaincodeStubInterface) peer.Response {
+	return shim.Success([]byte("Chaincode initialized"))
+}
+
+/*
+ * This function will be called when user query or add any user data
+ */
+func (contract *MyContract) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+
+	fun, args := stub.GetFunctionAndParameters()
+
+	if fun == "addUser" {
+		return addUser(stub, args)
+	} else if fun == "getUser" {
+		return getUser(stub, args)
+	} else if fun == "listAllUsers" {
+		return listAllUsers(stub)
+	}
+
+	return shim.Error("Invalid Function name")
+}
+
+/**
+ * This function will add a user in ledger
+ */
+func addUser(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	if len(args) != 3 {
+		return shim.Error("Required args ==> 3")
+	}
+	//convert rollno to int
+	rollNo, err := strconv.ParseInt(args[0], 10, 64)
+
+	if err != nil {
+		fmt.Println("Error while converting rollno to int ", err)
+		return shim.Error("Error while converting rollno to int")
+	}
+
+	fmt.Println("Rollno is ==> ", rollNo)
+
+	user := User{RollNo: rollNo, FirstName: args[1], LastName: args[2]}
+
+	userBytes, err := json.Marshal(user)
+
+	if err != nil {
+		fmt.Println("Error while converting json", err)
+	}
+	stub.PutState(args[0], userBytes)
+
+	return shim.Success([]byte("Rollno converted to int"))
+
+	return shim.Success([]byte("Done"))
+}
+
+/*
+ * This function will fetch the user from statedb
+ */
+func getUser(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	if len(args) != 1 {
+		return shim.Error("Please provide a user id")
+	}
+
+	byteValue, err := stub.GetState(args[0])
+
+	if err != nil {
+		return shim.Error(err.Error())
+	} else if len(byteValue) == 0 {
+		return shim.Error("Record Not Found")
+	}
+
+	return shim.Success(byteValue)
+}
+
+func listAllUsers(stub shim.ChaincodeStubInterface) peer.Response {
+
+	queryIterator, err := stub.GetQueryResult("{\"selector\":{}}")
+
+	if err != nil {
+		fmt.Println("Error occured ==> ", err)
+		return shim.Error(err.Error())
+	}
+
+	var buffer bytes.Buffer
+
+	buffer.WriteString("[")
+
+	isFirstRecord := true
+
+	for queryIterator.HasNext() {
+		queryResult, _ := queryIterator.Next()
+
+		if !isFirstRecord {
+			buffer.WriteString(",")
+		}
+
+		buffer.WriteString("{\"key\":")
+		buffer.WriteString("\"")
+
+		buffer.WriteString(queryResult.Key)
+
+		buffer.WriteString("\",")
+		buffer.WriteString("\"value\":")
+		buffer.WriteString(string(queryResult.Value))
+		buffer.WriteString("}")
+
+		isFirstRecord = false
+	}
+
+	buffer.WriteString("]")
+	fmt.Println("Records ==> ", buffer.String())
+
+	return shim.Success([]byte(buffer.String()))
+}
+
+func main() {
+	err := shim.Start(new(MyContract))
+
+	if err != nil {
+		fmt.Printf("Error ==> %s", err)
+	}
+}
--- setup-peer.sh
+++ setup-peer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CHANNEL_NAME=channel1
+CHANNEL_NAME=channel1
 
 echo "******************SET CHANNEL_NAME=channel1******************"
 
@@ -16,16 +16,16 @@ CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypt
 
 echo "******************Peer 0 Of Axis Joined Channel1******************"
 
-peer channel update -o hr.debut.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/DebutMSPanchors.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/debut.com/orderers/hr.debut.com/msp/tlscacerts/tlsca.debut.com-cert.pem
+# peer channel update -o hr.debut.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/DebutMSPanchors.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/debut.com/orderers/hr.debut.com/msp/tlscacerts/tlsca.debut.com-cert.pem
 
 echo "******************DEBUT MSP ANCHOR PEER CHANNEL UPDATED******************"
 
-CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/axisbank.com/users/Admin@axisbank.com/msp CORE_PEER_ADDRESS=peer0.axisbank.com:7051 CORE_PEER_LOCALMSPID=AxisMSP CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/axisbank.com/peers/peer0.axisbank.com/tls/ca.crt peer channel update -o hr.debut.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/AxisMSPanchors.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/debut.com/orderers/hr.debut.com/msp/tlscacerts/tlsca.debut.com-cert.pem
+# CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/axisbank.com/users/Admin@axisbank.com/msp CORE_PEER_ADDRESS=peer0.axisbank.com:7051 CORE_PEER_LOCALMSPID=AxisMSP CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/axisbank.com/peers/peer0.axisbank.com/tls/ca.crt peer channel update -o hr.debut.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/AxisMSPanchors.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/debut.com/orderers/hr.debut.com/msp/tlscacerts/tlsca.debut.com-cert.pem
 
 echo "******************AXIS MSP ANCHOR PEER CHANNEL UPDATED******************"
 
 
- peer chaincode install -n cc -v 1.0 -p github.com/chaincode/fabcar/go/
+ peer chaincode install -n cc -v 1.0 -p github.com/chaincode/
 
  echo "******************CHAINCODE cc 1.0 INSTALLED******************"
 
@@ -33,7 +33,20 @@ echo "******************AXIS MSP ANCHOR PEER CHANNEL UPDATED******************"
 
 echo "******************CHAINCODE cc 1.0 INSTANTIATED******************"
 
+peer chaincode invoke -o hr.debut.com:7050  --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/debut.com/orderers/hr.debut.com/msp/tlscacerts/tlsca.debut.com-cert.pem  -C $CHANNEL_NAME -n mychaincode -c '{"Function":"addUser","Args":["1","Akshay","Sood"]}'
 
-peer chaincode invoke -o hr.debut.com:7050  --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/debut.com/orderers/hr.debut.com/msp/tlscacerts/tlsca.debut.com-cert.pem  -C $CHANNEL_NAME -n cc -c '{"Args":["initLedger"]}'
+# sleep 5
 
-peer chaincode invoke -o hr.debut.com:7050  --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/debut.com/orderers/hr.debut.com/msp/tlscacerts/tlsca.debut.com-cert.pem  -C $CHANNEL_NAME -n cc -c '{"Args":["queryAllCars"]}'
+# peer chaincode invoke -o hr.debut.com:7050  --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/debut.com/orderers/hr.debut.com/msp/tlscacerts/tlsca.debut.com-cert.pem  -C $CHANNEL_NAME -n cc -c '{"Args":["initLedger"]}'
+
+# sleep 5
+
+# peer chaincode invoke -o hr.debut.com:7050  --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/debut.com/orderers/hr.debut.com/msp/tlscacerts/tlsca.debut.com-cert.pem  -C $CHANNEL_NAME -n cc -c '{"Args":["queryAllCars"]}'
+
+# sleep 2
+
+# sudo apt-get update
+
+# sleep 10
+
+# sudo apt-get install curl
